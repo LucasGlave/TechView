010a45fa2dba312003244ab46f42b53041b90a29
Lucas Glave <lucasglave7@gmail.com>
2023-07-05 10:58:48 -0300

    ads

--- src/componentes/CuelloDeBotella/CuelloProcesador.js
+++ src/componentes/CuelloDeBotella/CuelloProcesador.js
@@ -318,9 +318,9 @@ class CuelloProcesador extends React.Component {
             )}
             <div className="volverEntrar">
               <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4820714602088067"
-                  crossorigin="anonymous"></script>
-              <ins class="adsbygoogle"
-                  style="display:block"
+                  crossOrigin="anonymous"></script>
+              <ins className="adsbygoogle"
+                  style={{display:'block'}}
                   data-ad-format="fluid"
                   data-ad-layout-key="-6t+ed+2i-1n-4w"
                   data-ad-client="ca-pub-4820714602088067"
